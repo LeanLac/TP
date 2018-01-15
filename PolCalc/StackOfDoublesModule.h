e272a66ea391c5254dbd5447ea613f1651b1c8b5
LeanLac <leandrolacave@gmail.com>
2018-01-15 00:03:49 -0300

    Create StackOfDoublesModule.h

---
+++ PolCalc/StackOfDoublesModule.h
@@ -0,0 +1,10 @@
+#ifndef STACKOFDOUBLESMODULE_H
+#define STACKOFDOUBLESMODULE_H
+
+typedef double StackItem;
+StackItem pop(void);
+void push(StackItem item);
+int isEmpty(void);
+int isFull(void);
+
+#endif
